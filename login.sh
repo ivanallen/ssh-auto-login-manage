f322ebec66b9ad07b03b4657be3fd91c04db9188
liu.feng <liu.feng@xsky.com>
2023-10-12 09:33:23 +0800

    modify hosts
    Signed-off-by: liu.feng <liu.feng@xsky.com>

--- login.sh
+++ login.sh
@@ -8,8 +8,11 @@ user=$3
 password=$4
 id_file=$5
 
+export GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+
 if [ $5 ]; then
     ./auto_login_with_id_file.exp $host $port $user $id_file
 else
 	./auto_login.exp $host $port $user $password
 fi
+
